be524f2543415497b287dff2609f1097cf612a47
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-18 16:24:47 -0300

    Add Data Persistance

--- src/main/java/com/car/api/controller/CarController.java
+++ src/main/java/com/car/api/controller/CarController.java
@@ -1,19 +1,24 @@
 package com.car.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.car.api.dto.CarDTO;
+import com.car.api.model.Car;
+import com.car.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/car")
 public class CarController {
+
+  @Autowired
+  private CarRepository repository;
+
   @PostMapping
   public void postCar(@RequestBody CarDTO req) {
-    System.out.println(req);
-    System.out.println("Ano do Modelo: " + req.anoModelo());
-    System.out.println("Ano de Fabricação: " + req.dataFabricacao());
+    repository.save(new Car(req));
   }
 }
---
+++ src/main/java/com/car/api/model/Car.java
@@ -0,0 +1,42 @@
+package com.car.api.model;
+
+import com.car.api.dto.CarDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Car {
+
+  public Car(CarDTO data){
+    this.modelo = data.modelo();
+    this.fabricante = data.fabricante();
+    this.dataFabricacao = data.dataFabricacao();
+    this.valor = data.valor();
+    this.anoModelo = data.anoModelo();
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 40, nullable = false)
+  private String modelo; 
+
+  @Column(length = 40, nullable = false)
+  private String fabricante; 
+  
+  @Column(length = 40, nullable = false)
+  private String dataFabricacao; 
+
+  @Column(nullable = false)
+  private Double valor;
+
+  @Column(nullable = false)
+  private int anoModelo;
+}
